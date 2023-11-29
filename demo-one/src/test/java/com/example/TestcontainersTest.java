c8f00819eb632d38d63a259eacf6206e6eb2082e
Sakibul_Munna <sakibul.munnaa@gmail.com>
2023-11-29 10:08:57 +0600

    Test container test only.

--- demo-one/src/test/java/com/example/TestcontainersTest.java
+++ demo-one/src/test/java/com/example/TestcontainersTest.java
@@ -1,38 +1,16 @@
 package com.example;
 
-import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.Test;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Testcontainers
-public class TestcontainersTest {
-
-    @Container
-    private static final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:latest")
-            .withDatabaseName("amigosecode-dao-unit-test")
-            .withUsername("root")
-            .withPassword("root");
+public class TestcontainersTest extends AbstractTestcontainers {
 
     @Test
     void canStartPostgresDB() {
         assertThat(postgreSQLContainer.isRunning()).isTrue();
         assertThat(postgreSQLContainer.isCreated()).isTrue();
-    }
-
-    @Test
-    void canApplyDBMigrationsWithFlyway() {
-        Flyway flyway = Flyway.configure().dataSource(
-                postgreSQLContainer.getJdbcUrl(),
-                postgreSQLContainer.getUsername(),
-                postgreSQLContainer.getPassword()
-        ).load();
-        flyway.migrate();
-        System.out.println();
     }
 }
