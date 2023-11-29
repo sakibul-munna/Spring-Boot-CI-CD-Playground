5186b8a1bedf55d740bc9ae05198b3d066ab6996
Sakibul_Munna <sakibul.munnaa@gmail.com>
2023-11-29 09:32:46 +0600

    Two sample test cases regarding starting the container and database inside the container.

---
+++ demo-one/src/test/java/com/example/TestcontainersTest.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+public class TestcontainersTest {
+
+    @Container
+    private static final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:latest")
+            .withDatabaseName("amigosecode-dao-unit-test")
+            .withUsername("root")
+            .withPassword("root");
+
+    @Test
+    void canStartPostgresDB() {
+        assertThat(postgreSQLContainer.isRunning()).isTrue();
+        assertThat(postgreSQLContainer.isCreated()).isTrue();
+    }
+
+    @Test
+    void canApplyDBMigrationsWithFlyway() {
+        Flyway flyway = Flyway.configure().dataSource(
+                postgreSQLContainer.getJdbcUrl(),
+                postgreSQLContainer.getUsername(),
+                postgreSQLContainer.getPassword()
+        ).load();
+        flyway.migrate();
+        System.out.println();
+    }
+}
