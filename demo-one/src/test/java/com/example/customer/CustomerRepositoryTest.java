a6dc31ddce8a3cfa52f77e66c0f24917919d113a
Sakibul_Munna <sakibul.munnaa@gmail.com>
2023-12-03 09:42:11 +0600

    Test cases for customer repository.

---
+++ demo-one/src/test/java/com/example/customer/CustomerRepositoryTest.java
@@ -0,0 +1,69 @@
+package com.example.customer;
+
+import com.example.AbstractTestcontainers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class CustomerRepositoryTest extends AbstractTestcontainers {
+
+    @Autowired
+    private CustomerRepository underTest;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void existsCustomerByEmail() {
+        String email = FAKER.internet().safeEmailAddress() + "-" + UUID.randomUUID();
+        Customer customer = new Customer(
+                FAKER.name().fullName(),
+                email,
+                20
+        );
+        underTest.save(customer);
+
+        boolean actual = underTest.existsCustomerByEmail(email);
+
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void existsCustomerWithEmailReturnsFalseWhenEmailNotExist() {
+        String email = FAKER.internet().safeEmailAddress() + "-" + UUID.randomUUID();
+
+        boolean actual = underTest.existsCustomerByEmail(email);
+
+        assertThat(actual).isFalse();
+    }
+
+    @Test
+    void deleteById() {
+        String email = FAKER.internet().safeEmailAddress() + "-" + UUID.randomUUID();
+        Customer customer = new Customer(
+                FAKER.name().fullName(),
+                email,
+                20
+        );
+        underTest.save(customer);
+
+        Long id = underTest.findAll().stream().filter(c ->
+                c.getEmail().equals(email)
+        ).map(Customer::getId).findFirst().orElseThrow();
+
+        underTest.deleteById(id);
+
+        Optional<Customer> actual = underTest.findById(id);
+        assertThat(actual).isEmpty();
+    }
+}
