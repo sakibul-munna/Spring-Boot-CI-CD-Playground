d9f3904e76d6084b971bd5b367ad17920ded032a
Sakibul_Munna <sakibul.munnaa@gmail.com>
2023-12-03 16:48:05 +0600

    Customer Row Mapper unit testing.

---
+++ demo-one/src/test/java/com/example/customer/CustomerRowMapperTest.java
@@ -0,0 +1,29 @@
+package com.example.customer;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class CustomerRowMapperTest {
+
+    @Test
+    void mapRow() throws SQLException {
+        CustomerRowMapper customerRowMapper = new CustomerRowMapper();
+        ResultSet resultSet = mock(ResultSet.class);
+        when(resultSet.getLong("id")).thenReturn(1L);
+        when(resultSet.getInt("age")).thenReturn(19);
+        when(resultSet.getString("name")).thenReturn("Jamila");
+        when(resultSet.getString("email")).thenReturn("test@gmail.com");
+
+        Customer actual = customerRowMapper.mapRow(resultSet, 1);
+
+        Customer expected = new Customer(1L, "Jamila", "test@gmail.com", 19);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}
