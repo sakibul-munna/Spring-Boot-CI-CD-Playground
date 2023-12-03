f02b6f2bfc7d5dc0aeea6fe0e6efbdeda223f88f
Sakibul_Munna <sakibul.munnaa@gmail.com>
2023-12-03 16:47:19 +0600

    Unit testing of JPA Access Service.

---
+++ demo-one/src/test/java/com/example/customer/CustomerJPADataAccessServiceTest.java
@@ -0,0 +1,79 @@
+package com.example.customer;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.verify;
+
+class CustomerJPADataAccessServiceTest {
+
+    private CustomerJPADataAccessService underTest;
+    private AutoCloseable autoCloseable;
+    @Mock private CustomerRepository customerRepository;
+
+    @BeforeEach
+    void setUp() {
+        autoCloseable = MockitoAnnotations.openMocks(this);
+        underTest = new CustomerJPADataAccessService(customerRepository);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        autoCloseable.close();
+    }
+
+    @Test
+    void selectAllCustomers() {
+        underTest.selectAllCustomers();
+
+        verify(customerRepository).findAll();
+    }
+
+    @Test
+    void selectCustomerById() {
+        long id = 1;
+
+        underTest.selectCustomerById(id);
+
+        verify(customerRepository).findById(id);
+    }
+
+    @Test
+    void insertCustomer() {
+        Customer customer = new Customer("Test", "test@gmail.com", 22);
+
+        underTest.insertCustomer(customer);
+
+        verify(customerRepository).save(customer);
+    }
+
+    @Test
+    void existsCustomerWithEmail() {
+        String testEmail = "test01@gmail.com";
+
+        underTest.existsCustomerWithEmail(testEmail);
+
+        verify(customerRepository).existsCustomerByEmail(testEmail);
+    }
+
+    @Test
+    void deleteCustomerById() {
+        Long id = 1L;
+
+        underTest.deleteCustomerById(id);
+
+        verify(customerRepository).deleteById(id);
+    }
+
+    @Test
+    void updateCustomer() {
+        Customer customer = new Customer("Test", "test@gmail.com", 22);
+
+        underTest.updateCustomer(customer);
+
+        verify(customerRepository).save(customer);
+    }
+}
