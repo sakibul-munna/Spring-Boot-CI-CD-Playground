9407d3d63ed12df727afb9a9da00c5ce781b5c4a
Sakibul_Munna <sakibul.munnaa@gmail.com>
2023-12-24 10:38:08 +0600

    Fixed the URL issue.

--- back-end/src/main/java/com/blockcraft/PingPongController.java
+++ back-end/src/main/java/com/blockcraft/PingPongController.java
@@ -1,9 +1,11 @@
 package com.blockcraft;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/dummy")
+@RestController
+@RequestMapping("api/dummy")
 public class PingPongController {
 
     record PingPong(String result) {
